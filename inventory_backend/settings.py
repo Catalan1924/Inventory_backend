3bf6f27da9d34dad45c6e36ca77f0e36e6f9e45d
Mesh😁 <oduor2shem@gmail.com>
2025-12-04 08:04:40 +0300

    fix: Update ALLOWED_HOSTS to include the production URL

--- inventory_backend/settings.py
+++ inventory_backend/settings.py
@@ -18,8 +18,7 @@ DEBUG = os.environ.get("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.environ.get(
     "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
-    "https://inventory-backend-1-kcep.onrender.com"
+    "127.0.0.1,localhost,inventory-backend-1-kcep.onrender.com"
 ).split(",")
 
 
